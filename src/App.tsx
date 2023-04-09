1ff6eca08be6f9950dc02604a1eadce20f9f724e
junaidn <junaidnaseer137@gmail.com>
2023-04-09 23:34:14 +0500

    removed unncessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import '../src/styles/main.scss';
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <h1>Start Creating component</h1>
+      <h1>2FA App</h1>
     </div>
   );
 };
