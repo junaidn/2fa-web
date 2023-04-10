c0490f55d95dd6794d587371a56fbc3949384d0d
junaidn <junaidnaseer137@gmail.com>
2023-04-10 20:13:53 +0500

    add header component

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import {Home} from './Home';
 import {Layout} from './components/Layout/Layout';
+import {Header} from './components/Header/Header';
 
 export const AppRouter = () => {
   return (
-    <Layout>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </Layout>
+    <>
+      <Header />
+      <Layout>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </Layout>
+    </>
   );
 };
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <header className="header">
+        <div className="header__container">
+            <h3>Logo</h3>
+            <h3>Icon</h3>
+        </div>
+    </header>
+  );
+};
