ef3308ecf8b978ac19170c551a5c718c3985341d
junaidn <junaidnaseer137@gmail.com>
2023-04-11 22:30:19 +0500

    add input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,29 @@
+import React, {FC} from 'react';
+
+interface IProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  isError?: boolean;
+  helpText?: string;
+}
+
+export const Input: FC<IProps> = ({
+  label,
+  type = 'text',
+  isError = false,
+  helpText = '',
+  placeholder,
+  ...props
+}) => {
+  return (
+    <div className="input-container">
+      <label className="input-container__label">{label}</label>
+      <input
+        className={isError ? 'input-error' : ''}
+        type={type}
+        placeholder={placeholder}
+        {...props}
+      />
+      {isError && <label className="error-text">{helpText}</label>}
+    </div>
+  );
+};
